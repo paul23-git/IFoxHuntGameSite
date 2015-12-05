2afd6ad98b6d7bd1667ac8fb381a8fc22f2661f3
pulli23 <pweijtens@gmail.com>
2015-12-05 15:32:26 +0100

    delete nginx

--- IFoxHuntGame/game_funcs.py
+++ IFoxHuntGame/game_funcs.py
@@ -5,8 +5,7 @@ def haversine(lon1, lat1, lon2, lat2):
     Calculate the great circle distance between two points
     on the earth (specified in decimal degrees)
     """
-    # convert decimal degrees to radians
-    print((lon1, lat1, lon2, lat2))
+    # convert decimal degrees to radiansF
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
     # haversine formula
--- IFoxHuntGame/views.py
+++ IFoxHuntGame/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#import logging
 # Create your views here.
 
 from .models import *
@@ -11,28 +12,28 @@ from django.views.decorators.csrf import csrf_protect, csrf_exempt
 
 @csrf_exempt
 def index(request):
-    print(" -----------------------  ")
     if request.method=="GET":
         d = request.GET
     else:
         d = request.POST
-    print("input: ", d)
     try:
         inName = d["name"]
         inPass = d["pass"]
     except KeyError as e:
-        return HttpResponse("")
-    print(inName, inPass)
+        r = HttpResponse("Invalid config")
+        r.status_code = 403;
+        return  r;
     try:
         mygroup = Group.objects.get(name=inName,password=inPass)
     except:
-        return HttpResponse("")
+        r = HttpResponse("E:Incorrect Pass-Name combination")
+        r.status_code = 403;
+        return r;
     if request.method == "GET":
         all_others = Group.objects.exclude(hunter=mygroup.hunter)
         #others_str = ';'.join([p.send_data() for p in all_others])
         target = min((haversine(mygroup.longitude,mygroup.latitude, o.longitude, o.latitude),o)
                      for o in all_others)
-        print(target)
         others_str = target[1].send_data()
         if mygroup.hunter:
             exclude = 2
@@ -42,20 +43,27 @@ def index(request):
         powerups_str = ';'.join([repr(p) for p in all_powerups])
         return HttpResponse(others_str + '\n' + powerups_str)
     else:
-        position = d["pos"]
-        l = position.split(",")
-        mygroup.longitude = float(l[0])
-        mygroup.latitude = float(l[1])
-        mygroup.altitude = float(l[2])
-        mygroup.save()
-        if mygroup.hunter:
-            exclude = 2
-        else:
-            exclude = 1
-        all_powerups = PowerUp.objects.exclude(who=exclude)
         try:
-            d = [p for p in all_powerups if haversine(mygroup.longitude, mygroup.latitude, p.longitude, p.latitude) < 10][0]
-            d.delete()
-            return HttpResponse(repr(d))
+            position = d["pos"]
+            print(position)
+            l = position.split(",")
+            mygroup.longitude = float(l[0])
+            mygroup.latitude = float(l[1])
+            mygroup.altitude = float(l[2])
+            mygroup.save()
+            if mygroup.hunter:
+                exclude = 2
+            else:
+                exclude = 1
+            all_powerups = PowerUp.objects.exclude(who=exclude)
+            all_picked_powerups = [p for p in all_powerups if haversine(mygroup.longitude, mygroup.latitude, p.longitude, p.latitude) < 10]
+            print("PU")
+            if len(all_picked_powerups) > 0:
+                picked_powerup = all_picked_powerups[0];
+                return HttpResponse(picked_powerup)
+            else:
+                return HttpResponse("")
         except:
-            return HttpResponse("")
+            r =  HttpResponse("Bad format")
+            r.status_code = 400;
+            return r;
